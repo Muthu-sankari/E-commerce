5bb0ac652a4e85b1301cf14b92bf33d47df5b26c
Muthu-sankari <muthushaboo@gmail.com>
2024-11-30 21:05:42 +0530

    .

--- my-shopping-app/src/Pages/Shop.jsx
+++ my-shopping-app/src/Pages/Shop.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Header from '../components/Header/Header'
+import Popular from '../components/Popular/Popular'
+import Offers from '../components/Offers/Offers'
 
 const Shop = () => {
   return (
     <div>
       <Header/>
+      <Popular/>
+      <Offers/>
     </div>
   )
 }
--- my-shopping-app/src/components/Item/Item.jsx
+++ my-shopping-app/src/components/Item/Item.jsx
@@ -8,10 +8,10 @@ const Item = (props) => {
         <p>{props.name}</p>
         <div className='Item-prices'>
         <div className='Item-price-new'>
-            {props.new_price}
+            ${props.new_price}
         </div>
         <div className='Item-price-old'>
-        {props.old_price}
+        ${props.old_price}
         </div>
     </div>
     </div>
