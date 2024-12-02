7146e3af9624951d231b05ca2bf0dbaa6d917a86
Muthu-sankari <muthushaboo@gmail.com>
2024-12-02 22:22:21 +0530

    ..

--- my-shopping-app/src/components/Assests/new_collections.js
+++ my-shopping-app/src/components/Assests/new_collections.js
@@ -6,8 +6,7 @@ import p5_img from "./product_15.png";
 import p6_img from "./product_2.png";
 import p7_img from "./product_17.png";
 import p8_img from "./product_28.png";
-
-let new_collections = [
+let new_collections =[
   {
     id: 12,
     name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
