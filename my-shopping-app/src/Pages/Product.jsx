4dadc8a1cdde3fb194d819de9e5745e2fe3f541e
Muthu-sankari <muthushaboo@gmail.com>
2024-12-17 14:26:12 +0530

    ..

--- my-shopping-app/src/Pages/Product.jsx
+++ my-shopping-app/src/Pages/Product.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import {ShopContext} from '../Context/ShopContext'
+import {useParams} from 'react-router-dom'
+import Breadcrum from '../components/Assests/Breadcrums/Breadcrum';
+import ProductDisplay from '../components/Assests/ProductDisplay/ProductDisplay';
 const Product = () => {
   const {all_product}=useContext(ShopContext);
   const {productId }=useParams();
-  const product=all_product.find((e)=>e.id===Number(productId)
+  const product=all_product.find((e)=>e.id===Number(productId));
   return (
-    <div>Product</div>
+    <div>
+<Breadcrum product={product}/>
+<ProductDisplay product={product}/>
+
+    </div>
   )
 }
 
